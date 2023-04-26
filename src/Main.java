b9189a0cf03dace0e40d863e821ce859860b67c1
Queren Hapuque <queren.fernandes@yahoo.com>
2023-04-26 14:37:54 -0300

    primeiros conceitos

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.8) /3;
+        System.out.println(media);
+
+        String sinopse;
+        sinopse = "Filme de aventura com galã dos anos 80";
+        System.out.println(sinopse);
+        }
+    }
