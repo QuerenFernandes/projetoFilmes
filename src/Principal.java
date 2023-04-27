a3c0e106b56a5082079a329d33d777149ec8b6f8
Queren Hapuque <queren.fernandes@yahoo.com>
2023-04-27 07:13:29 -0300

    comecando oo

--- src/Principal.java
+++ src/Principal.java
@@ -1,18 +1,20 @@
+import br.com.alura.projetofilmes.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "Eragon";
-        meuFilme.anoDeLancamento = 2006;
-        meuFilme.duracaoEmMinutos = 180;
-        meuFilme.categoriaDoFilme = "Fantasia";
+        meuFilme.setNome("Eragon");
+        meuFilme.setAnoDeLancamento(2006);
+        meuFilme.setDuracaoEmMinutos(180);
+        meuFilme.setCategoriaDoFilme("Fantasia");
+
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(9);
         meuFilme.avalia(10);
         meuFilme.avalia(0);
 
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
     }
 }
---
+++ src/br/com/alura/projetofilmes/modelos/Filme.java
@@ -0,0 +1,76 @@
+package br.com.alura.projetofilmes.modelos;
+
+public class Filme {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+    private String categoriaDoFilme;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean getIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public String getCategoriaDoFilme() {
+        return categoriaDoFilme;
+    }
+
+    public int getTotalDeAvaliacoes(){
+        return totalDeAvaliacoes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public void setCategoriaDoFilme(String categoriaDoFilme) {
+        this.categoriaDoFilme = categoriaDoFilme;
+    }
+
+    public void exibeFichaTecnica() {
+        System.out.println("Nome do br.com.alura.projetofilmes.modelos.Filme: " + nome);
+        System.out.println("Ano de Lançamento do br.com.alura.projetofilmes.modelos.Filme: " + anoDeLancamento);
+    }
+
+    public void avalia(double nota){
+        somaDasAvaliacoes = somaDasAvaliacoes + nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+
+
+}
+
+// public: Uma classe, atributo ou método declarado como public pode ser acessado por qualquer classe em qualquer pacote.
+// private: só pode ser acessado dentro da própria classe
+// default: não especifica nenhum modificador de acesso, pode ser acessado apenas pelas classes que estão no mesmo pacote
+
