e4960ce269c69a377f3792e2ba2dd143c4b06e0a
Queren Hapuque <queren.fernandes@yahoo.com>
2023-04-27 20:06:59 -0300

    OO - Polimorfismo

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,6 @@
+import br.com.alura.projetofilmes.calculo.CalculadoraDeTempo;
 import br.com.alura.projetofilmes.modelos.Filme;
+import br.com.alura.projetofilmes.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -16,5 +18,29 @@ public class Principal {
 
         System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
+
+        Serie serie = new Serie();
+        serie.setNome("Cake Boss");
+        serie.setAnoDeLancamento(2017);
+        serie.setEpisodiosPorTemporada(8);
+        serie.setTemporadas(10);
+        serie.setMinutosPorTemporada(20);
+        System.out.println("Duração para maratonar: " + serie.getNome() + ": " + serie.getDuracaoEmMinutos() + " minutos.");
+
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Senhor dos Aneis");
+        outroFilme.setAnoDeLancamento(2000);
+        outroFilme.setDuracaoEmMinutos(200);
+        outroFilme.setCategoriaDoFilme("Fantasia");
+
+
+        CalculadoraDeTempo calculadoraDeTempo = new CalculadoraDeTempo();
+        calculadoraDeTempo.inclui(meuFilme);
+        calculadoraDeTempo.inclui(outroFilme);
+        calculadoraDeTempo.inclui(serie);
+        System.out.println(calculadoraDeTempo.getTempoTotal());
+
     }
 }
---
+++ src/br/com/alura/projetofilmes/modelos/Serie.java
@@ -0,0 +1,46 @@
+package br.com.alura.projetofilmes.modelos;
+
+public class Serie extends Titulo {
+    private int temporadas;
+    private boolean ativa;
+    private int episodiosPorTemporada;
+    private int minutosPorTemporada;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public boolean getAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public int getMinutosPorTemporada() {
+        return minutosPorTemporada;
+    }
+
+    public void setMinutosPorTemporada(int minutosPorTemporada) {
+        this.minutosPorTemporada = minutosPorTemporada;
+    }
+
+    //Override possibilita a sobrescrita de um método
+    @Override
+    public int getDuracaoEmMinutos() {
+        return temporadas * episodiosPorTemporada * minutosPorTemporada;
+    }
+}
